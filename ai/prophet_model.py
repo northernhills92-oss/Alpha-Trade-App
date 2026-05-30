e72efb0133fefc99de80916425f5d3d65ff6c484
northernhills92-oss <northernhills92@gmail.com>
2026-05-30 21:59:45 +0800

    prophet_model.py

--- ai/prophet_model.py
+++ ai/prophet_model.py
@@ -1,1 +1,30 @@
+from prophet import Prophet
 
+
+def prophet_signal(df):
+
+    model_df = df[['Date', 'Close']].rename(
+        columns={
+            'Date': 'ds',
+            'Close': 'y'
+        }
+    )
+
+    model = Prophet(
+        daily_seasonality=True,
+        changepoint_prior_scale=0.05
+    )
+
+    model.fit(model_df)
+
+    future = model.make_future_dataframe(periods=24)
+
+    forecast = model.predict(future)
+
+    current = forecast['yhat'].iloc[-2]
+    future_price = forecast['yhat'].iloc[-1]
+
+    if future_price > current:
+        return "BUY"
+
+    return "SELL"
