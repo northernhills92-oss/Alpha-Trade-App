fd8d9a8bbfdf51e80d43a1d7b81c2d8001a5478b
northernhills92-oss <northernhills92@gmail.com>
2026-05-30 21:16:00 +0800

    indicators.py

--- core/indicator.py
+++ core/indicator.py
@@ -1,1 +1,26 @@
+import ta
 
+
+def add_indicators(df):
+
+    df['RSI'] = ta.momentum.RSIIndicator(
+        close=df['Close'],
+        window=14
+    ).rsi()
+
+    df['EMA20'] = ta.trend.EMAIndicator(
+        close=df['Close'],
+        window=20
+    ).ema_indicator()
+
+    df['EMA50'] = ta.trend.EMAIndicator(
+        close=df['Close'],
+        window=50
+    ).ema_indicator()
+
+    macd = ta.trend.MACD(close=df['Close'])
+
+    df['MACD'] = macd.macd()
+    df['MACD_SIGNAL'] = macd.macd_signal()
+
+    return df
