58e45ba33494daaed9297206a28915ba905ba8b9
northernhills92-oss <northernhills92@gmail.com>
2026-06-01 15:52:28 +0800

    Update notifications.py

--- utils/notifications.py
+++ utils/notifications.py
@@ -1,1 +1,21 @@
 
+import requests
+from config import (
+    TELEGRAM_TOKEN,
+    TELEGRAM_CHAT_ID
+)
+
+
+def send_telegram(message):
+
+    url = (
+        f"https://api.telegram.org/bot"
+        f"{TELEGRAM_TOKEN}/sendMessage"
+    )
+
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
+    }
+
+    requests.post(url, data=payload)
