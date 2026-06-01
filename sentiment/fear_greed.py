5e8207046daa29166fdb8dfa70c7097f87452d72
northernhills92-oss <northernhills92@gmail.com>
2026-06-01 15:48:31 +0800

    Add function to retrieve fear and greed index

--- sentiment/fear_greed.py
+++ sentiment/fear_greed.py
@@ -1,1 +1,18 @@
+import requests
 
+
+def get_fear_greed():
+
+    url = "https://api.alternative.me/fng/"
+
+    data = requests.get(url).json()
+
+    value = int(data['data'][0]['value'])
+
+    if value < 30:
+        return "FEAR"
+
+    if value > 70:
+        return "GREED"
+
+    return "NEUTRAL"
