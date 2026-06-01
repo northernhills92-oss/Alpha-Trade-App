07263939faeb803e78e7b2dcc7ca4c671c975d59
northernhills92-oss <northernhills92@gmail.com>
2026-06-01 15:49:11 +0800

    Update whale_tracker.py

--- whale/whale_tracker.py
+++ whale/whale_tracker.py
@@ -1,1 +1,15 @@
+import random
 
+
+def detect_whale_activity():
+
+    spike = random.choice([
+        True,
+        False,
+        False
+    ])
+
+    if spike:
+        return "WHALE ACTIVITY DETECTED"
+
+    return "NORMAL"
