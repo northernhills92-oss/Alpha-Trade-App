02709d84f9ad2a8fc085bc2a2f9338b2240fda9b
northernhills92-oss <northernhills92@gmail.com>
2026-06-01 15:51:23 +0800

    Update journal.py

--- core/journal.py
+++ core/journal.py
@@ -1,1 +1,28 @@
+import pandas as pd
+from datetime import datetime
 
+
+LOG_FILE = "trade_journal.csv"
+
+
+def log_trade(asset, signal, price):
+
+    row = {
+        "time": datetime.now(),
+        "asset": asset,
+        "signal": signal,
+        "price": price
+    }
+
+    try:
+        df = pd.read_csv(LOG_FILE)
+
+    except:
+        df = pd.DataFrame()
+
+    df = pd.concat([
+        df,
+        pd.DataFrame([row])
+    ])
+
+    df.to_csv(LOG_FILE, index=False)
